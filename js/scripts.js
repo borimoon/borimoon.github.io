828bcfb8cb9e2a3c96399fee868de2cd7a0289da
borimoon <adrienn.ficzere@gmail.com>
2017-07-17 16:41:04 +0200

    ...

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// mobile menu show-hide
+$(function() {
+	var menuVisible = false;
+	$('#toggle').click(function() {
+		if (menuVisible) {
+			$('#menu').css({'display':'none'});
+			menuVisible = false;
+			return;
+		}
+		$('#menu').css({'display':'block'});
+		menuVisible = true;
+	});
+	$('#menu').click(function() {
+		$(this).css({'display':'none'});
+		menuVisible = false;
+	});
+});
+
+// menu open
+$(document).ready(function(){
+	$('#toggle').click(function(){
+		$(this).toggleClass('menuopen');
+	});
+
+	$('#menu').click(function(){
+		$('#toggle').removeClass('menuopen');
+	});
+});
+
+
+// stop youtube video
+$(function(){
+	$('.stop').click(function(){
+		$('iframe').attr('src', $('iframe').attr('src'));
+	});
+});
