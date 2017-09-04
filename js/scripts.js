66eda14a42efedc437a20941a418e8f3b3ad9e29
borimoon <adrienn.ficzere@gmail.com>
2017-09-04 14:15:01 +0200

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -38,61 +38,17 @@ $(function() {
 	$('#toggleOn').click(function() {
 		if (menuVisible) {
 			$('#menu').css({'display':'none'});
+			$('#toggleOn').removeClass('menuopen');
 			menuVisible = false;
 			return;
 		}
 		$('#menu').css({'display':'block'});
+		$('#toggleOn').toggleClass('menuopen');
 		menuVisible = true;
 	});
 	$('#menu').click(function() {
 		$(this).css({'display':'none'});
+		$('#toggleOn').removeClass('menuopen');
 		menuVisible = false;
 	});
 });
-
-// slider
-$.global = new Object();
-
-$.global.item = 1;
-$.global.total = 0;
-
-$(document).ready(function()
-	{
-
-	var WindowWidth = $('.slider').width();
-	var SlideCount = $('.carousel li').length;
-	var SlidesWidth = SlideCount * WindowWidth;
-
-   $.global.item = 0;
-    $.global.total = SlideCount;
-
-  $('.prev').click(function() { Slide('back'); });
-  $('.next').click(function() { Slide('forward'); });
-
-  });
-
-function Slide(direction)
-	{
-
-    if (direction == 'back') { var $target = $.global.item - 1; }
-    if (direction == 'forward') { var $target = $.global.item + 1; }
-
-    if ($target == -1) { DoIt($.global.total-1); }
-    else if ($target == $.global.total) { DoIt(0); }
-    else { DoIt($target); }
-
-	}
-
-function DoIt(target)
-	{
-
-	  var $windowwidth = $('.slider').width();
-		var $margin = $windowwidth * target;
-	  var $actualtarget = target+1;
-
-	  $(".carousel li:nth-child("+$actualtarget+")").addClass('alive');
-
-	  $('.carousel').css('transform','translate3d(-'+$margin+'px,0px,0px)');
-
-	  $.global.item = target;
-	}
