87deb8c7405458ef8fe7335d101c31d6ef5d5f74
borimoon <adrienn.ficzere@gmail.com>
2018-04-18 17:54:51 +0200

    ...

---
+++ recement/js/scripts.js
@@ -0,0 +1,47 @@
+// mobile menu show-hide
+$(function() {
+  var menuVisible = false;
+  $('#toggle').click(function() {
+    if (menuVisible) {
+      $('#menu').css({'display':'none'});
+      $('#toggle').removeClass('menuopen');
+      menuVisible = false;
+      return;
+    }
+    $('#menu').css({'display':'block'});
+    $('#toggle').toggleClass('menuopen');
+    menuVisible = true;
+  });
+  $('#menu').click(function() {
+    $(this).css({'display':'none'});
+    $('#toggle').removeClass('menuopen');
+    menuVisible = false;
+  });
+});
+
+// animate scroll down
+
+$(document).ready(function (){
+    $('.to-service').click(function (){
+        $('html, body').animate({
+            scrollTop: $('.service').offset().top
+        }, 700);
+    });
+});
+
+$(document).ready(function (){
+    $('.to-contact').click(function (){
+        $('html, body').animate({
+            scrollTop: $('.contact').offset().top
+        }, 700);
+    });
+});
+
+// animate scroll to top
+$(document).ready(function (){
+    $('.to-top').click(function (){
+        $('html, body').animate({
+            scrollTop: $('.uvp').offset().top
+        }, 700);
+    });
+});
